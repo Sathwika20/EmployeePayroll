e761b0a26770a2dc05fed838ac062aef485677d1
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-03-29 19:10:48 +0530

    Added code to connect JDBC to mysql

--- src/main/java/com/bridgelabz/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -1,23 +1,22 @@
 package com.bridgelabz;
-
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 public class EmployeePayroll {
     private static final String URL = "jdbc:mysql://localhost:3306/payroll_services?useSSL=false";
     private static final String user = "root";
     private static final String password = "sathWIKA@20";
 
-    private void establishConnection() throws EmployeePayrollException {
-        Connection connection;
+    private Connection establishConnection() {
+        Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver found!");
         } catch (ClassNotFoundException e) {
-            throw new EmployeePayrollException("Cannot find the JDBC Driver!!", EmployeePayrollException.ExceptionType.CANNOT_LOAD_DRIVER);
+            e.printStackTrace();
         }
         listDrivers();
         try {
@@ -25,9 +24,28 @@ public class EmployeePayroll {
             connection = DriverManager.getConnection(URL, user, password);
             System.out.println("Connection established with: " + connection);
         } catch (SQLException e) {
-            throw new EmployeePayrollException("Cannot connect to the JDBC Driver!!",
-                    EmployeePayrollException.ExceptionType.WRONG_CREDENTIALS);
+            e.printStackTrace();
         }
+        return connection;
+    }
+
+    public List<EmployeePayrollData> readData() {
+        String sql = "SELECT * FROM employee_payroll;";
+        List<EmployeePayrollData> employeePayrollData = new ArrayList<>();
+        try (Connection connection = this.establishConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double salary = resultSet.getDouble("salary");
+                LocalDate startDate = resultSet.getDate("start").toLocalDate();
+                employeePayrollData.add(new EmployeePayrollData(id, name, salary, startDate));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employeePayrollData;
     }
 
     private void listDrivers() {
@@ -36,11 +54,6 @@ public class EmployeePayroll {
             Driver driverClass = driverList.nextElement();
             System.out.println("Driver: " + driverClass.getClass().getName());
         }
-    }
-
-    public static void main(String[] args) throws EmployeePayrollException {
-        EmployeePayroll employeePayroll = new EmployeePayroll();
-        employeePayroll.establishConnection();
     }
 
 
---
+++ src/test/java/com/bridgelabz/EmployeePayrollTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
+
+
+public class EmployeePayrollTest {
+    @Test
+    public void givenEmployeePayrollDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayroll employeePayroll = new EmployeePayroll();
+        List<EmployeePayrollData> employeePayrollDataList = employeePayroll.readData();
+        Assert.assertEquals(3, employeePayrollDataList.size());
+    }
+
+}
