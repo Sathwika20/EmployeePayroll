d5e8a8cce905c47932ceba438788d147f8e93854
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-03-30 20:33:38 +0530

    Added code to connect JDBC to mysql

--- src/test/java/com/bridgelabz/EmployeePayrollTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollTest.java
@@ -2,29 +2,12 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.time.LocalDate;
 import java.util.List;
 
 
 public class EmployeePayrollTest {
-    @Test
-    public void givenEmployeePayrollDB_WhenRetrieved_ShouldMatchEmployeeCount() {
-        EmployeePayroll employeePayroll = new EmployeePayroll();
-        List<EmployeePayrollData> employeePayrollDataList = employeePayroll.readData();
-        Assert.assertEquals(3, employeePayrollDataList.size());
-    }
-    @Test
-    public void givenNewSalaryForEmployee_WhenUpdated_ShouldReturn1() {
-        EmployeePayroll employeePayroll = new EmployeePayroll();
-        employeePayroll.readData();
-        int salaryUpdated= employeePayroll.updateEmployeeDataUsingStatement();
-        Assert.assertEquals(1, salaryUpdated);
-    }
-    @Test
-    public void givenNewSalaryForEmployee_WhenUpdatedUsingPreparedStatement_ShouldReturn1() {
-        EmployeePayroll employeePayroll = new EmployeePayroll();
-        employeePayroll.readData();
-        int salaryUpdated= employeePayroll.updateEmployeeDataUsingPreparedStatement("Charlie", 3000000);
-        Assert.assertEquals(1, salaryUpdated);
-    }
+
 
 }
