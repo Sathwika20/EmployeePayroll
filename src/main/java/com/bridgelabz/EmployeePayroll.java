e9376cce5928116f6b74d81ef8b02c070c2091b7
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-03-31 18:44:18 +0530

    Added code to connect JDBC to mysql

--- src/main/java/com/bridgelabz/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -1,10 +1,11 @@
 package com.bridgelabz;
 
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Enumeration;
-import java.util.List;
 
 public class EmployeePayroll {
 
@@ -12,49 +13,34 @@ public class EmployeePayroll {
     private static final String user = "root";
     private static final String password = "sathWIKA@20";
 
-        private Connection establishConnection(){
-            Connection connection = null;
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                System.out.println("Driver found!");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            listDrivers();
-            try {
-                System.out.println("\nConnecting to database: " + URL);
-                connection = DriverManager.getConnection(URL, user, password);
-                System.out.println("Connection established with: " + connection);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return connection;
+    private void establishConnection() {
+        Connection connection;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver found!");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+        listDrivers();
+        try {
+            System.out.println("\nConnecting to database: " + URL);
+            connection = DriverManager.getConnection(URL, user, password);
+            System.out.println("Connection established with: " + connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-        public List<EmployeePayrollData> readData() {
-            String sql = "SELECT * FROM employee_payroll;";
-            List<EmployeePayrollData> employeePayrollData = new ArrayList<>();
-            try (Connection connection = this.establishConnection()) {
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(sql);
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("id");
-                    String name = resultSet.getString("name");
-                    double salary = resultSet.getDouble("salary");
-                    LocalDate startDate = resultSet.getDate("start").toLocalDate();
-                    employeePayrollData.add(new EmployeePayrollData(id, name, salary, startDate));
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return employeePayrollData;
+    private void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = driverList.nextElement();
+            System.out.println("Driver: " + driverClass.getClass().getName());
         }
+    }
 
-        private void listDrivers() {
-            Enumeration<Driver> driverList = DriverManager.getDrivers();
-            while (driverList.hasMoreElements()) {
-                Driver driverClass = driverList.nextElement();
-                System.out.println("Driver: " + driverClass.getClass().getName());
-            }
-        }
+    public static void main(String[] args) {
+        EmployeePayroll employeePayroll = new EmployeePayroll();
+        employeePayroll.establishConnection();
+    }
 }
